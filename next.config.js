70c876a903d13633f7f605a8a677ab62d54d63cc
Mona Lisa <elbadaouylotfelisslam@gmail.com>
2024-11-02 03:21:43 +0100

    feqt; pwa icons

--- next.config.js
+++ next.config.js
@@ -7,8 +7,4 @@ const withPWA = require('next-pwa')({
 
 module.exports = withPWA({
   reactStrictMode: true,
-  images: {
-    // Configure image domains if you are using images
-    domains: ['example.com'],
-  },
 });
